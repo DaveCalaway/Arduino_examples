34106a6cbc24f905884b79b9b167ca0c1a64cca7
Dave <davide.gariselli@gmail.com>
2017-10-19 17:51:03 +0200

    up

--- 7segmenti/2_sette_segmenti/2_sette_segmenti.ino
+++ 7segmenti/2_sette_segmenti/2_sette_segmenti.ino
@@ -1,52 +1,142 @@
-// sette segmenti array
-
-int num_array[10][7] = {  { 1,1,1,1,1,1,0 },    // 0
-                          { 0,1,1,0,0,0,0 },    // 1
-                          { 1,1,0,1,1,0,1 },    // 2
-                          { 1,1,1,1,0,0,1 },    // 3
-                          { 0,1,1,0,0,1,1 },    // 4
-                          { 1,0,1,1,0,1,1 },    // 5
-                          { 1,0,1,1,1,1,1 },    // 6
-                          { 1,1,1,0,0,0,0 },    // 7
-                          { 1,1,1,1,1,1,1 },    // 8
-                          { 1,1,1,0,0,1,1 }};   // 9
-                                       
-//function header
-void Num_Write(int);
-
-void setup() 
-{ 
-  // set pin modes
-  pinMode(2, OUTPUT);   
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
+// Sette segmenti evoluto
+// made by: Prof. Michele Maffucci  
   
- 
-}
-
-void loop() 
-{
+// Conteggio 9 secondi con display 7 segmenti a catodo comune  
   
-  //counter loop
+// definizione delle variabili a cui collegare ogni segmento del display  
+// la lettera rappresenta il segmento del display il numero il pin  
+// digitale di Arduino a cui è collegato il corrispondente pin del display  
   
-  for (int counter = 10; counter > 0; --counter) 
-  {
-   delay(1000);
-   Num_Write(counter-1); 
-  }
-  delay(3000);
-}
+const int a=7;  
+const int b=6;  
+const int c=4;  
+const int d=2;  
+const int e=1;  
+const int f=9;  
+const int g=10;  
+  
+void LedAcceso(int n)  
+{  
+  switch(n)  
+  {  
+  case 0:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, HIGH);   
+    digitalWrite(e, HIGH);  
+    digitalWrite(f, HIGH);  
+    digitalWrite(g, LOW);  
+    break;  
+  
+  case 1:  
+    digitalWrite(a, LOW);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, LOW);   
+    digitalWrite(e, LOW);  
+    digitalWrite(f, LOW);  
+    digitalWrite(g, LOW);  
+    break;  
+  
+  case 2:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, LOW);   
+    digitalWrite(d, HIGH);   
+    digitalWrite(e, HIGH);  
+    digitalWrite(f, LOW);  
+    digitalWrite(g, HIGH);  
+    break;  
+  case 3:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, HIGH);   
+    digitalWrite(e, LOW);  
+    digitalWrite(f, LOW);  
+    digitalWrite(g, HIGH);  
+    break;  
+  
+  case 4:  
+    digitalWrite(a, LOW);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, LOW);   
+    digitalWrite(e, LOW);  
+    digitalWrite(f, HIGH);  
+    digitalWrite(g, HIGH);  
+    break;  
+  
+  case 5:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, LOW);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, HIGH);   
+    digitalWrite(e, LOW);  
+    digitalWrite(f, HIGH);  
+    digitalWrite(g, HIGH);  
+    break;  
+  
+  case 6:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, LOW);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, HIGH);   
+    digitalWrite(e, HIGH);  
+    digitalWrite(f, HIGH);  
+    digitalWrite(g, HIGH);  
+    break;  
+  
+  case 7:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, LOW);   
+    digitalWrite(e, LOW);  
+    digitalWrite(f, LOW);  
+    digitalWrite(g, LOW);  
+    break;  
+  
+  case 8:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, HIGH);   
+    digitalWrite(e, HIGH);  
+    digitalWrite(f, HIGH);  
+    digitalWrite(g, HIGH);  
+    break;  
+  
+  case 9:  
+    digitalWrite(a, HIGH);  
+    digitalWrite(b, HIGH);  
+    digitalWrite(c, HIGH);   
+    digitalWrite(d, HIGH);   
+    digitalWrite(e, LOW);  
+    digitalWrite(f, HIGH);  
+    digitalWrite(g, HIGH);  
+    break;  
+  }  
+};  
+  
+void setup()  
+{  
+  pinMode(a, OUTPUT);   
+  pinMode(b, OUTPUT);   
+  pinMode(c, OUTPUT);   
+  pinMode(d, OUTPUT);   
+  pinMode(e, OUTPUT);   
+  pinMode(f, OUTPUT);   
+  pinMode(g, OUTPUT);  
+}  
+  
+void loop ()  
+{  
+  for (int i=0;i<10;i++)   
+  {  
+    LedAcceso(i);  
+    delay (1000);  
+  }  
+}  
 
-// this functions writes values to the sev seg pins  
-void Num_Write(int number) 
-{
-  int pin= 2;
-  for (int j=0; j < 7; j++) {
-   digitalWrite(pin, num_array[number][j]);
-   pin++;
-  }
-}
