ee8ed7a2230dc6ecb1163a6dc97eba1fddac1c80
Dave <davide.gariselli@gmail.com>
2018-01-16 09:57:37 +0100

    frequency adjustment added

--- LoRa/rf95_client/rf95_client.ino
+++ LoRa/rf95_client/rf95_client.ino
@@ -1,5 +1,9 @@
 // rf95_client.pde
 // -*- mode: C++ -*-
+
+//Writed by Mike McCauley
+//Edited by DaveCalaway - https://goo.gl/FpTxsu -
+
 // Example sketch showing how to create a simple messageing client
 // with the RH_RF95 class. RH_RF95 class does not provide for addressing or
 // reliability, so you should only use RH_RF95 if you do not need the higher
@@ -14,33 +18,53 @@
 // Singleton instance of the radio driver
 RH_RF95 rf95;
 //RH_RF95 rf95(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
-//RH_RF95 rf95(8, 3); // Adafruit Feather M0 with RFM95 
+//RH_RF95 rf95(8, 3); // Adafruit Feather M0 with RFM95
 
 // Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
 //#define Serial SerialUSB
 
-void setup() 
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+#define led 13
+
+void setup()
 {
   // Rocket Scream Mini Ultra Pro with the RFM95W only:
   // Ensure serial flash is not interfering with radio communication on SPI bus
-//  pinMode(4, OUTPUT);
-//  digitalWrite(4, HIGH);
+  //  pinMode(4, OUTPUT);
+  //  digitalWrite(4, HIGH);
 
+  pinMode(led, OUTPUT);
   Serial.begin(9600);
-  while (!Serial) ; // Wait for serial port to be available
-  if (!rf95.init())
+  //while (!Serial) ; // Wait for serial port to be available
+  if (!rf95.init()) { // Module not connected well
     Serial.println("init failed");
+    digitalWrite(led, 1);
+    delay(250);
+    digitalWrite(led, 0);
+    delay(250);
+  }
+
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  if (!rf95.setFrequency(RF95_FREQ)) { // Module change Frequency
+    Serial.println("setFrequency failed");
+    digitalWrite(led, 1);
+    delay(250);
+    digitalWrite(led, 0);
+    delay(250);
+  }
+  digitalWrite(led, 1); // Module works well
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
-//  driver.setTxPower(23, false);
+  //  driver.setTxPower(23, false);
   // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
   // transmitter RFO pins and not the PA_BOOST pins
-  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
+  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
   // Failure to do that will result in extremely low transmit powers.
-//  driver.setTxPower(14, true);
+  //  driver.setTxPower(14, true);
 }
 
 void loop()
@@ -49,21 +73,21 @@ void loop()
   // Send a message to rf95_server
   uint8_t data[] = "Hello World!";
   rf95.send(data, sizeof(data));
-  
+
   rf95.waitPacketSent();
   // Now wait for a reply
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
 
   if (rf95.waitAvailableTimeout(3000))
-  { 
-    // Should be a reply message for us now   
+  {
+    // Should be a reply message for us now
     if (rf95.recv(buf, &len))
-   {
+    {
       Serial.print("got reply: ");
       Serial.println((char*)buf);
-//      Serial.print("RSSI: ");
-//      Serial.println(rf95.lastRssi(), DEC);    
+      //      Serial.print("RSSI: ");
+      //      Serial.println(rf95.lastRssi(), DEC);
     }
     else
     {
--- LoRa/rf95_server/rf95_server.ino
+++ LoRa/rf95_server/rf95_server.ino
@@ -1,5 +1,9 @@
 // rf95_server.pde
 // -*- mode: C++ -*-
+
+//Writed by Mike McCauley
+//Edited by DaveCalaway - https://goo.gl/FpTxsu -
+
 // Example sketch showing how to create a simple messageing server
 // with the RH_RF95 class. RH_RF95 class does not provide for addressing or
 // reliability, so you should only use RH_RF95  if you do not need the higher
@@ -14,60 +18,76 @@
 // Singleton instance of the radio driver
 RH_RF95 rf95;
 //RH_RF95 rf95(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
-//RH_RF95 rf95(8, 3); // Adafruit Feather M0 with RFM95 
+//RH_RF95 rf95(8, 3); // Adafruit Feather M0 with RFM95
 
 // Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
 //#define Serial SerialUSB
 
-int led = 13;
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
 
-void setup() 
+#define led 13
+
+void setup()
 {
   // Rocket Scream Mini Ultra Pro with the RFM95W only:
   // Ensure serial flash is not interfering with radio communication on SPI bus
-//  pinMode(4, OUTPUT);
-//  digitalWrite(4, HIGH);
+  //  pinMode(4, OUTPUT);
+  //  digitalWrite(4, HIGH);
 
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);
   Serial.begin(9600);
-  while (!Serial) ; // Wait for serial port to be available
-  if (!rf95.init())
-    Serial.println("init failed");  
+  if (!rf95.init()) { // Module not connected well
+    Serial.println("init failed");
+    digitalWrite(led, 1);
+    delay(250);
+    digitalWrite(led, 0);
+    delay(250);
+  }
+
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  if (!rf95.setFrequency(RF95_FREQ)) { // Module change Frequency
+    Serial.println("setFrequency failed");
+    digitalWrite(led, 1);
+    delay(250);
+    digitalWrite(led, 0);
+    delay(250);
+  }
+  digitalWrite(led, 1); // Module works well
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
-//  driver.setTxPower(23, false);
+  //  driver.setTxPower(23, false);
   // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
   // transmitter RFO pins and not the PA_BOOST pins
-  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
+  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
   // Failure to do that will result in extremely low transmit powers.
-//  driver.setTxPower(14, true);
+  //  driver.setTxPower(14, true);
 }
 
 void loop()
 {
   if (rf95.available())
   {
-    // Should be a message for us now   
+    // Should be a message for us now
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len))
     {
       digitalWrite(led, HIGH);
-//      RH_RF95::printBuffer("request: ", buf, len);
+      //      RH_RF95::printBuffer("request: ", buf, len);
       Serial.print("got request: ");
       Serial.println((char*)buf);
-//      Serial.print("RSSI: ");
-//      Serial.println(rf95.lastRssi(), DEC);
-      
+      //      Serial.print("RSSI: ");
+      //      Serial.println(rf95.lastRssi(), DEC);
+
       // Send a reply
       uint8_t data[] = "And hello back to you";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
       Serial.println("Sent a reply");
-       digitalWrite(led, LOW);
+      digitalWrite(led, LOW);
     }
     else
     {
